20f06e7a2a74598f2aec8466c812abb71bf2c692
Toiio21 <zukkoiq@gmail.com>
2021-06-01 20:13:34 +0300

    routing and navigate HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import HomePage from './components/HomePage/HomePage';
 import './App.css';
+import { Route, Switch } from 'react-router';
+
+const HatsPage = () => {
+  return <div>HATS PAGE</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
